6131092e2e4fd63fd7612051c0dd651fdf37804f
Khushi-codingal <rashmiabc@gmail.com>
2025-11-03 18:48:09 +0530

    average

---
+++ activity1.py
@@ -0,0 +1,13 @@
+#storing values 
+n1 = 90
+n2 = 45
+n3 = 65
+n4 = 67
+n5 = 89
+
+#finding the total sum
+sum = n1+n2+n3+n4+n5
+print("total sum is:", sum)
+#finding average
+average = sum/5
+print("average is:", average)
